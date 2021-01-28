84fa17a464e4184674b3b9ec229d0c72c3117a20
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-28 14:13:03 -0300

    Lidando com operadores lógicos encadeados

--- condicionais.js
+++ condicionais.js
@@ -7,15 +7,25 @@ const listaDeDestinos = new Array(
 )
 
 const idadeComprador = 17;
+const estaAcompanhada = true;
+const temPassagemComprada = true;
 
 console.log("DESTINOS POSSÍVEIS");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
-    listaDeDestinos.splice(1, 1);//remover se for maior de idade
+if (idadeComprador >= 18 || estaAcompanhada == true) { 
+    console.log("Boa Viagem!!");
+    listaDeDestinos.splice(2,1);
 } else {
-    console.log("COMPRADOR MENOR DE IDADE");
+    console.log("Não é maior de idade e não posso vender");
 }
- 
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada == true){
+    console.log("Boa viagem");
+} else{
+   console.log("Você não pode embarcar");
+} 
+
 console.log("DESTINOS POSSÍVEIS");
 console.log(listaDeDestinos);
