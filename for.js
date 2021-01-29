db9a17d474dfa08db2cd0c172d61073e1b3cabc2
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-29 18:33:54 -0300

    Atualizando exemplode for

--- for.js
+++ for.js
@@ -18,17 +18,11 @@ const podeComprar =  idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
 let destinoExiste = false
-while(contador<3){
-
-    if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe");
-        destinoExiste = true;
-        break;
-    }
-    contador += 1;
-}        
 
 for(let cont = 0 ; cont <3 ; cont++){
     if(listaDeDestinos[contador] == destino){
         destinoExiste = true;
+        console.log("Destino existe");
+        break;
+}
 }
