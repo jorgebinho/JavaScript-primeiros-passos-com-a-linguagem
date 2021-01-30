94886569e0e2fae467796b1f5e0ef0ceb10c00a2
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-29 23:36:56 -0300

    Criando operações de saque e depósito

--- jsoo/index.js
+++ jsoo/index.js
@@ -12,6 +12,12 @@ class ContaCorrente{
             this.saldo -= valor;
         } 
     }
+
+    depositar(valor){
+        if(valor > 0){
+            this.saldo += valor; 
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -28,11 +34,13 @@ const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.saldo = 0
 contaCorrenteRicardo.agencia = 1001;
 console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
 
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(200);
+contaCorrenteRicardo.depositar(-1);
 contaCorrenteRicardo.sacar(50);
 
 console.log(contaCorrenteRicardo.saldo);
 
-console.log(cliente1,cliente2);
+//console.log(cliente1,cliente2);
