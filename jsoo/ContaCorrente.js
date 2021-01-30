da9bd4b6bc83aace9b2b03d7fb1c43e64abb6153
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-30 02:18:35 -0300

    Acessando atributos privados

--- jsoo/ContaCorrente.js
+++ jsoo/ContaCorrente.js
@@ -1,25 +1,46 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
     agencia;
-    cliente;
+    _cliente;
 
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+    
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
-        if (this._saldo >= valor) {
+        if(this._saldo >= valor){
             this._saldo -= valor;
             return valor;
-        } 
-    }
-
-    depositar(valor) {
-        if(valor <= 0) {
-            return;
         }
-        this._saldo += valor;
     }
 
-    transferir(valor, conta){
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+
+    tranferir(valor, conta){
+        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        
     }
 }
--- jsoo/index.js
+++ jsoo/index.js
@@ -2,10 +2,12 @@ import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
 
-cliente1.nome = "José";
-cliente1.cpf = 11122233345;
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -13,12 +15,10 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 88822233309;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
 let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
+contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log(conta2);
+console.log(conta2.saldo);
