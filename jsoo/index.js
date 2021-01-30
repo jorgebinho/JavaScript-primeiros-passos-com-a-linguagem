82f8c8f8e24fe57e71208a8659fc04c648e1d496
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-29 23:13:07 -0300

    Criando a segunda classe

--- jsoo/index.js
+++ jsoo/index.js
@@ -1,8 +1,17 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        } 
+    }
 }
 
 const cliente1 = new Cliente();
@@ -10,12 +19,20 @@ const cliente2 = new Cliente();
 
 cliente1.nome = "José";
 cliente1.cpf = 11122233345;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233345;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteRicardo = new ContaCorrente();
+
+contaCorrenteRicardo.saldo = 0
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo.saldo);
 
 console.log(cliente1,cliente2);
