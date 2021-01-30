ab086298e6e44213c6b797dd83d182df78ecc5ff
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-30 01:29:32 -0300

    Atualizando o index

--- jsoo/index.js
+++ jsoo/index.js
@@ -7,19 +7,18 @@ const cliente2 = new Cliente();
 cliente1.nome = "José";
 cliente1.cpf = 11122233345;
 
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233345;
-
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 88822233309;
 conta2.agencia = 102;
 
-contaCorrenteRicardo.transferir(200, conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
 console.log(conta2);
