8f7a995c34979eed1a95a83a2dbf0a1f3220ca5c
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-30 20:29:13 -0300

    Lidando com polimorfismo

--- jsoo/index.js
+++ jsoo/index.js
@@ -1,11 +1,13 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./Conta/ContaCorrente.js"
-import {ContaPoupanca} from "./Conta/ContaPoupanca.js"
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("1234567");
+const gerente = new Gerente("Ricardo", 5000, 12345678901);
+gerente.cadastrarSenha("123")
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const estaLogado = SistemaAutenticacao.login(gerente, "123");
 
-console.log(contaCorrenteRicardo);
-console.log(contaPoupanca);
+console.log(estaLogado);
