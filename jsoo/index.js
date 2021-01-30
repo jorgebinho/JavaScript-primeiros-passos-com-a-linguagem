71705f3f6a31191995810a1434b13c7fce3516c1
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-30 20:04:32 -0300

    Modularizando as classes Contas e Funcionarios

--- jsoo/index.js
+++ jsoo/index.js
@@ -1,7 +1,6 @@
 import {Cliente} from "./Cliente.js"
-import { Conta } from "./Conta.js";
-import {ContaCorrente} from "./ContaCorrente.js"
-import {ContaPoupanca} from "./ContaPoupanca.js"
+import {ContaCorrente} from "./Conta/ContaCorrente.js"
+import {ContaPoupanca} from "./Conta/ContaPoupanca.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
